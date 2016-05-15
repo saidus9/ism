d8367c39863f2d3c58eb94baab75dac35c91ce8b
saidus9 <sac@example.com>
2016-05-15 12:53:53 +0100

    Recreated DB and migrations
    Create new DB and ran migrations

--- migrations/20160514164126_create_user_table.php
+++ migrations/20160514164126_create_user_table.php
@@ -21,12 +21,15 @@ class CreateUserTable extends AbstractMigration
     {
       $users = $this->table('users');
       $users->addColumn('first_name', 'string')
-            ->addColumn('last_name', 'string')
-            ->addColumn('email', 'string')
-            ->addColumn('password', 'string')
-            ->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
-            ->addColumn('updated_at', 'datetime', ['null' => true])
-            ->save();
+          ->addColumn('last_name', 'string')
+          ->addColumn('email', 'string')
+          ->addColumn('password', 'string')
+          ->addColumn('active', 'integer', ['default' => '0'])
+          ->addColumn('access_level', 'integer', ['default' => '1'])
+          ->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+          ->addColumn('updated_at', 'datetime', ['null' => true])
+          ->addIndex(['email'], ['unique' => true])
+          ->save();
     }
 
     public function down()
--- migrations/20160514171537_create_pages_table.php
+++ migrations/20160514171537_create_pages_table.php
@@ -6,11 +6,13 @@ class CreatePagesTable extends AbstractMigration
 {
   public function up()
   {
-    $users = $this->table('pages');
-    $users->addColumn('browsertitle', 'string')
+      $users = $this->table('pages');
+      $users->addColumn('browser_title', 'string')
           ->addColumn('page_content', 'text')
+          ->addColumn('slug', 'string', ['default' => ''])
           ->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
           ->addColumn('updated_at', 'datetime', ['null' => true])
+          ->addIndex(['slug'], ['unique' => true])
           ->save();
   }
 
