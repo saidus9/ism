2a46581b77fa608c6d8fdb8870a3d073d68b2b0f
saidus9 <sac@example.com>
2016-05-18 21:17:19 +0100

    Updated controllers

--- routes.php
+++ routes.php
@@ -8,4 +8,11 @@ $router->map('POST', '/register', 'Acme\controllers\RegisterController@postShowR
 
 $router->map('GET', '/login', 'Acme\controllers\RegisterController@getShowLoginPage', 'login');
 
+$router->map('GET', '/page-not-found', 'Acme\controllers\PageController@getShow404', '404');
+
+$router->map('GET', '/slug', function() {
+  $slug = new Cocur\Slugify\Slugify();
+  echo $slug->slugify('About-ISM');
+});
+
 $router->map('GET', '/[*]', 'Acme\controllers\PageController@getShowPage', 'generic_page');
